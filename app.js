05e7b0250db55e7eb702c34bcf366b260cfe0119
Manick <manick@Manicks-MacBook-Air.local>
2019-08-03 12:48:38 +0530

    minimal setup

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const devRun = require("./utils/devRun");
-const morgan = require("morgan");
 const app = express();
 
-devRun(() => app.use(morgan("dev")));
+// devRun(() => app.use(morgan("dev")));
 app.use((req, res) => {
   res.json({
     message: "Message"
@@ -12,7 +10,5 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  devRun(() => {
-    console.log(`Server Running at ${PORT}`);
-  });
+  console.log("Server running");
 });
