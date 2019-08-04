02fc3a935673909203d8a0c0defa5f3e56aa1ca5
Manick <manick@Manicks-MacBook-Air.local>
2019-08-04 16:29:24 +0530

    client basic design complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,43 @@
 import React from "react";
 import axios from "axios";
-import "./App.css";
+import styled from "styled-components";
+import styles from "./App.module.css";
 
+const Input = styled.input`
+  padding: 10px 12px;
+  font-size: 0.875rem;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  border-radius: 4px;
+  display: block;
+  width: 100%;
+  margin: 14px 0px;
+  outline: none;
+  :focus {
+    border-color: mediumseagreen;
+  }
+`;
+const Button = styled.button`
+  padding: 10px 12px;
+  border: 1px solid transparent;
+  font-size: 0.875rem;
+  border-radius: 4px;
+  margin-left: 10px;
+  background: mediumseagreen;
+  font-weight: 500;
+  text-transform: uppercase;
+  color: white;
+  cursor: pointer;
+  margin: 10px 0px;
+`;
+const URL = styled.p`
+  border: 1px solid green;
+  border-radius: 4px;
+  padding: 10px 30px;
+  color: green;
+  margin: 14px auto;
+  display: inline-block;
+`;
 class App extends React.Component {
   state = {
     shortUrl: ""
@@ -27,18 +63,23 @@ class App extends React.Component {
       .catch(err => {
         console.log(err);
       });
+    this.inputRef.current.value = "";
   };
   render() {
     return (
-      <div className="App">
-        <input
-          size="80"
-          type="text"
-          ref={this.inputRef}
-          placeholder="type url"
-        />
-        <button onClick={this.shortUrl}>Shorten</button>
-        <p>{this.state.shortUrl}</p>
+      <div className={styles.App}>
+        <div className={styles.Container}>
+          <Input
+            size="80"
+            type="text"
+            ref={this.inputRef}
+            placeholder="Enter Url you want to short"
+          />
+          <Button onClick={this.shortUrl}>Shorten</Button>
+          <div>
+            {this.state.shortUrl.length > 0 && <URL>{this.state.shortUrl}</URL>}
+          </div>
+        </div>
       </div>
     );
   }
