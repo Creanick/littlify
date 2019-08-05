dc413c6cb58eebd9ee344444eead44c071d0418c
Manick <manick@Manicks-MacBook-Air.local>
2019-08-05 08:06:05 +0530

    snackbar slide animation

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Snackbar from "@material-ui/core/Snackbar";
+import Slide from "@material-ui/core/Slide";
+function SlideTransition(props) {
+  return <Slide {...props} direction="up" />;
+}
 function Message({ open = false, message, onClose }) {
   return (
     <Snackbar
       anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
       open={open}
+      TransitionComponent={SlideTransition}
       autoHideDuration={2000}
       message={<span>{message}</span>}
       onClose={onClose}
